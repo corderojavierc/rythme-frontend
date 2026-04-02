80328aa0ed70a3eca8f81ff906ee02fa1f2474c7
corderojavierc <corderojavierc@gmail.com>
2026-04-02 12:45:03 +0200

    feat: bassic logout implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
+import { useAuth } from "./auth/AuthProvider";
+import { useNavigate } from "react-router-dom";
+
 export default function App() {
+    const auth = useAuth();
+    const navigate = useNavigate();
+
+    const handleLogout = async () => {
+        try {
+            const token = localStorage.getItem("token");
+
+            await fetch("http://127.0.0.1:8000/api/logout", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+
+            localStorage.removeItem("token");
+
+            auth.setIsAuthenticated(false);
+
+            navigate("/login");
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    };
+
     return (
         <div>
             <h1>Pantalla de App</h1>
+
+            <button onClick={handleLogout}>Cerrar sesión</button>
         </div>
     );
 }
