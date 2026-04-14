83506c840f89c020f35d3038dbff608f68e2bdf1
Javier Cordero <corderomartinjavier@gmail.com>
2026-04-14 13:15:53 +0200

    Revert "feat: added likes feature with and refactorized the code"

--- src/main.jsx
+++ src/main.jsx
@@ -3,45 +3,21 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App";
-import Login from "./routes/Login";
-import Register from "./routes/Register";
-import ProtectedRoute from "./routes/ProtectedRoute";
-import PublicRoute from "./routes/PublicRoute";
-import { AuthProvider } from "./auth/AuthProvider";
+import Login from "./Login";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <ProtectedRoute />,
-        children: [
-            {
-                path: "/",
-                element: <App />,
-            },
-        ],
+        element: <App />,
     },
     {
         path: "/login",
-        element: (
-            <PublicRoute>
-                <Login />
-            </PublicRoute>
-        ),
-    },
-    {
-        path: "/register",
-        element: (
-            <PublicRoute>
-                <Register />
-            </PublicRoute>
-        ),
+        element: <Login />,
     },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <AuthProvider>
-            <RouterProvider router={router} />
-        </AuthProvider>
+        <RouterProvider router={router} />
     </StrictMode>,
 );
